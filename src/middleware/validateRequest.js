8aec80ea7d43add179f2fc6a82153da553a22a51
Nuren7 <nurenislam01@gmail.com>
2026-09-19 12:15:20 +0100

    validators init

---
+++ src/middleware/validateRequest.js
@@ -0,0 +1,14 @@
+export const validateRequest = (schema) => {
+  return (req, res, next) => {
+    const result = schema.safePars(req.body);
+
+    if (!result.success) {
+      const errorMessages = result.error.errors.map((err) => err.message);
+      const error = errorMessages.join(", ");
+      return res.status(400).json({ message: error });
+    }
+
+
+    next()
+  };
+};
